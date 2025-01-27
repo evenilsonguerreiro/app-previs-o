3f9b5cea46c54193c857d56bfd11ca8d769bb132
evenilson <evenilsonguerreiro@gmail.com>
2025-01-27 19:29:34 -0300

    ajuste em tempo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const key = 'cebcd482eda57fa9a6714c1c2ba91885'
 
 function colocarDadosnatela(dados){
-    document.querySelector('.titulo-principal').innerHTML = 'Tem em ' + dados.name
+    document.querySelector('.titulo-principal').innerHTML = 'Tempo em ' + dados.name
     document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector('.descrição').innerHTML = dados.weather[0].description
     document.querySelector('.icone').src =  "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
